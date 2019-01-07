56821bc18a46f8cf299eb619eca69996b0582cd0
Kelly Sheddy <jungaka07@gmail.com>
2019-01-07 14:57:47 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,13 +1,14 @@
 class Backer
   attr_accessor :name 
+  @@all = []
   
   def initialize(name)
     @name = name
   end 
   
   def back_project(Project)
-    @@all = []
     @@all << Projects.new
+  end 
   
   
 end 
